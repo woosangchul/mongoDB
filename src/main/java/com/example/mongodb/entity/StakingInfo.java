b7e68d876ceca81d3a9dcd4514892e023c26c552
woosangchul <mye8072@naver.com>
2022-08-30 16:11:37 +0900

    update

--- src/main/java/com/example/mongodb/entity/StakingInfo.java
+++ src/main/java/com/example/mongodb/entity/StakingInfo.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.sql.Timestamp;
+
 @Data
 @Builder
 @Document(collection = "stackingInfo")
@@ -12,7 +14,14 @@ public class StakingInfo {
 
     @Id
     private String id;
+
+    //stack or unstack
     private String status;
 
+    private String name;
+
+    private String symbol;
+
+    private Timestamp timestamp;
 
 }
